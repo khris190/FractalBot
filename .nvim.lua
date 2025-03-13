12e8b95dbc619b6548b8879259991ddb4afde5e7
khris190 <khris10@interia.pl>
2025-03-13 20:41:50 +0100

    feat(wip): move to typescript

--- .nvim.lua
+++ .nvim.lua
@@ -2,7 +2,7 @@ require("lazyvim.util").lsp.on_attach(function(client)
 	if client.name == "ts_ls" then
 		client.server_capabilities.documentFormattingProvider = false
 	end
-	if client.name == "jsonls" then
-		client.server_capabilities.documentFormattingProvider = false
-	end
+	-- if client.name == "jsonls" then
+	-- 	client.server_capabilities.documentFormattingProvider = false
+	-- end
 end)
