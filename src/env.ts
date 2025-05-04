652b3bfc8040e434be2ef8a022932727095e3d39
khris190 <khris10@interia.pl>
2025-05-04 16:13:45 +0200

    feat: fix data paths

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ import path from 'node:path'
 const TOKEN = process.env.TOKEN
 const CLIENT_ID = process.env.CLIENT_ID
 const GUILD_ID = process.env.GUILD_ID
-const DATA_PATH = '/app/data'
+const DATA_PATH = process.env.DATA_PATH ?? './data'
 const DB_FILE = process.env.DB_FILE ?? 'db.sqlite'
 const DB_PATH = path.resolve(DATA_PATH, DB_FILE)
 export default {
