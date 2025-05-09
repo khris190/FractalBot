75b1e2c54d2c03476912496b8c7fac511bf61e0f
khris190 <khris10@interia.pl>
2025-05-09 19:17:17 +0200

    feat: print date and not only time in pretty logger target

--- src/logger/getLogger.ts
+++ src/logger/getLogger.ts
@@ -24,6 +24,7 @@ export default function getLogger (moduleName:string): ILogger {
   targets.push({
     stream: Pretty({
       destination: 1,
+      translateTime: 'yyyy-mm-dd HH:MM:ss.l'
     }),
     level: 'trace',
   })
