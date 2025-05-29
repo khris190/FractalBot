83242dafb2584855aff903761bc8845497cffc65
khris190 <khris10@interia.pl>
2025-05-29 10:10:11 +0200

    feat: rename param

--- src/commands/WriteStuffHereCommand.ts
+++ src/commands/WriteStuffHereCommand.ts
@@ -4,17 +4,19 @@ import client from '../Client'
 import { hiddenInteractionReply } from '../utils/helpers'
 import ReplyHelper from '../utils/ReplyHelper'
 
+const strOptionName = 'policyDefinitionSet'
+
 class WriteStuffHereCommand extends AdminChatCommand {
   constructor () {
     super('arbitrarytenets', 'tenets')
     this.data.addStringOption(option =>
-      option.setName('stuff').setDescription('test').setRequired(true)
+      option.setName(strOptionName).setDescription('test').setRequired(true)
     )
   }
 
   async run (interaction: ChatInputCommandInteraction): Promise<void> {
     const channelId = interaction.channelId
-    const content = interaction.options.getString('stuff')
+    const content = interaction.options.getString(strOptionName)
     hiddenInteractionReply(interaction, 'Done.')
     if (content) {
       const chan = client.client.channels.cache.get(channelId) as TextChannel
